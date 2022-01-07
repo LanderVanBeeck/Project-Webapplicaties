7b3210c85d960269f370599132397c7d95fe282c
LanderVanBeeck <landervanbeeck@hotmail.com>
2022-01-07 14:04:46 +0100

    Line-up page layout

--- Project_Webapplicaties/Controllers/LineUpController.cs
+++ Project_Webapplicaties/Controllers/LineUpController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_Webapplicaties.Data;
 using Project_Webapplicaties.Models;
+using Project_Webapplicaties.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +12,21 @@ namespace Project_Webapplicaties.Controllers
 {
     public class LineUpController : Controller
     {
-        public IActionResult Index()
+        public List<Artiest> artiesten { get; set; }
+
+        private readonly ProjectContext _context;
+
+        public LineUpController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
         {
             ViewData["Title"] = "Line-up";
-            return View();
+            LineUpListViewModel viewModel = new LineUpListViewModel();
+            viewModel.artiesten = await _context.Artiesten.ToListAsync();
+            return View(viewModel);
         }
     }
 }
---
+++ Project_Webapplicaties/ViewModels/LineUpListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Project_Webapplicaties.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Webapplicaties.ViewModels
+{
+    public class LineUpListViewModel
+    {
+        public List<Artiest> artiesten { get; set; }
+    }
+}
